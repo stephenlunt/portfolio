af7df11cc3f9b8dad204b52619fcecc0ee75905d
stephenlunt97 <stephenlunt@me.com>
2023-09-20 21:15:59 +0100

    feat: update page layouts and meta tags

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,1 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Stephen Lunt"
-export const SITE_DESCRIPTION = "Welcome to my website!"
